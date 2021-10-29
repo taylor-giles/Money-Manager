7c079d1878b8b34cfdc9fa0ed381360c49fc681c
taylor-giles <taylor.g1823@gmail.com>
2021-10-29 15:09:07 -0400

    Add edit budget activity

---
+++ app/src/main/java/giles/budgetapp/EditBudgetActivity.kt
@@ -0,0 +1,11 @@
+package giles.budgetapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditBudgetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_budget)
+    }
+}
